25e87f1e56fff0d71afb8e529bd14d9e6760c0fc
Asa Ayers <Asa@AsaAyers.com>
2021-08-27 09:04:16 -0700

    Planning tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,58 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
 import App from './App';
 
-describe('<App>', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
-  });
+const MAX_DENSITY = 50
+const MAX_SECTOR_SIZE = 25
+
+describe('Setup', () => {
+  // it('renders learn react link', () => {
+  //   const { getByText } = render(<App />);
+  //   const linkElement = getByText(/learn react/i);
+  //   expect(document.body.contains(linkElement));
+  // });
+
+  test('New Player: A user can submit thier name', () => {
+    const utils = render(<App />);
+    utils.findByLabelText('New Player')
+  })
+
+  test('The sector is regenerated when a player is added')
+
+  test('Sector: Changing the sector size changes the number of tiles', () => {
+
+  })
+  test('NumPlanets: changing the number of planets is reflected in the game board')
+  test('Start button is disabled until there are at least 2 players')
+  test('Clicking new sector shows a new game board')
+  test(
+    `Local Play: 3 player names can be entered, then start the game
+    The game boardtransfers to the new screen
+    `
+  )
+
+  describe('Error conditions', () => {
+    test('sector size cannot be negative')
+    describe(`Max Density: ${MAX_DENSITY}%`, () => {
+      test('sector size has a lower boundary')
+      test('numPlanets has an upper boundary')
+    })
+
+    test(`Max sector size: ${MAX_SECTOR_SIZE}`)
+
+
+    test('unclaimedPlanets:players ratio cannot be less than 3:1')
+    describe(`When a player is added that breaks the unclaimedPlanets ratio`, () => {
+      test(`then planets are added automatically`)
+
+      describe(`if the added planets breaks Max Density ${MAX_DENSITY}`, () => {
+        test('the sector size is increased automatically')
+      })
+    })
+
+  })
+
+
 });
